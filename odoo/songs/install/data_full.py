23aae21b6ce3e8014219eb545730da45e19745af
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-05-10 14:19:19 +0200

    Use xmlid for referencing categories
    The admin user has been changed to French... so it doesn't find the
    Expenses category.
    
    Please always use xmlids and not an implicit name_get!
    
    Also, I don't think changing the 'admin' user to be in French is
    something we want to do. If files are meant to be imported in French,
    just change the language in the context.
    
    Finally, I corrected the data files to load the product templates in
    every modes, not only the 'full' mode.

---
+++ odoo/songs/install/data_full.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import anthem
+
+""" Data loaded in full mode """
+
+
+@anthem.log
+def main(ctx):
+    """ Loading full data """
