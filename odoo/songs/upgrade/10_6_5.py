4eef4860c91c1ff673da185db4c41a0dc6f6ba2b
vrenaville <vincent.renaville@camptocamp.com>
2017-08-30 11:25:57 +0200

    [ADD] upgrade song

---
+++ odoo/songs/upgrade/10_6_5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import anthem
+from anthem.lyrics.records import create_or_update
+
+@anthem.log
+def create_mail_server(ctx):
+    """Prepare no accounting in holding"""
+    values = {
+        'name': "Mail Server",
+        }
+    create_or_update(ctx, 'fetchmail.server',
+                     '__setup__.mail_server', values)
+
+@anthem.log
+def main(ctx):
+    """ Run setup """
+    create_mail_server(ctx)
