913baab96efb156c381f1e3483af25ab78036f7c
CharlineDumontet <charline.dumontet@camptocamp.com>
2017-08-30 13:18:26 +0200

    Fix songs for merge in master

--- odoo/songs/upgrade/10_6_5.py
+++ odoo/songs/upgrade/10_6_5.py
@@ -3,6 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 import anthem
 from anthem.lyrics.records import create_or_update
+from ..common import req
+from base64 import b64encode
+from pkg_resources import resource_string
 
 
 @anthem.log
@@ -16,6 +19,29 @@ def create_mail_server(ctx):
 
 
 @anthem.log
+def remove_tagline(ctx):
+    """Remove tagline"""
+    company = ctx.env.ref('base.main_company')
+    company.write({'rml_header1': ''})
+
+
+@anthem.log
+def set_reports_logo(ctx):
+    """Set report_logo for all companies"""
+    main_company = ctx.env.ref('base.main_company')
+    logo_content = resource_string(
+        req, 'data/images/company_main_report_logo.png')
+    b64_logo = b64encode(logo_content)
+    main_company.report_logo = b64_logo
+
+    for company in ctx.env['res.company'].search(
+            [('id', '!=', main_company.id)]):
+        company.report_logo = company.logo
+
+
+@anthem.log
 def main(ctx):
     """ Run setup """
     create_mail_server(ctx)
+    remove_tagline(ctx)
+    set_reports_logo(ctx)
