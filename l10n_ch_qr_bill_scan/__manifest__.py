adb0cf931f67bcc9b6fc80d24d6b630bf5f19c8f
Maksym Yankin <yankinmk@gmail.com>
2021-04-30 11:47:36 +0300

    l10n_ch_qr_bill_scan: backport to v12
    [IMP] Add default supplier attribute to match with existing partner

--- l10n_ch_qr_bill_scan/__manifest__.py
+++ l10n_ch_qr_bill_scan/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020 Camptocamp SA
+# Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Switzerland - QR-bill scan",
     "summary": "Scan QR-bill to create vendor bills",
-    "version": "13.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "category": "Localization",
     "website": "https://github.com/OCA/l10n-switzerland",
